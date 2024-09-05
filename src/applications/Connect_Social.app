6f252fc6218ff368726a5f7b021cf44b04df4e73
gonetech <support@globalonetech.co.in>
2024-09-05 15:04:53 +0530

    src Commit

---
+++ src/applications/Connect_Social.app
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<CustomApplication xmlns="http://soap.sforce.com/2006/04/metadata">
+    <brand>
+        <headerColor>#0070D2</headerColor>
+        <shouldOverrideOrgTheme>false</shouldOverrideOrgTheme>
+    </brand>
+    <formFactors>Small</formFactors>
+    <formFactors>Large</formFactors>
+    <isNavAutoTempTabsDisabled>false</isNavAutoTempTabsDisabled>
+    <isNavPersonalizationDisabled>false</isNavPersonalizationDisabled>
+    <isNavTabPersistenceDisabled>false</isNavTabPersistenceDisabled>
+    <isOmniPinnedViewEnabled>false</isOmniPinnedViewEnabled>
+    <label>Connect Social</label>
+    <navType>Standard</navType>
+    <tabs>Connect_Social_Settings</tabs>
+    <tabs>Message_Template__c</tabs>
+    <tabs>Message__c</tabs>
+    <tabs>SMS_WhatsApp_Settings__c</tabs>
+    <tabs>Create_WhatsApp_Template</tabs>
+    <uiType>Lightning</uiType>
+    <utilityBar>Connect_Social_UtilityBar</utilityBar>
+</CustomApplication>
---
+++ src/lwc/chatComponent/constant.js
@@ -0,0 +1,36 @@
+import OPTION_SMS_LABEL from '@salesforce/label/c.Option_SMS_label';
+import OPTION_WHATSAPP_LABEL from '@salesforce/label/c.Option_WhatsApp_label';
+import SAVE_LABEL from '@salesforce/label/c.Save_label';
+import CANCEL_LABEL from '@salesforce/label/c.Cancel_label';
+import CLOSE_LABEL from '@salesforce/label/c.Close_label';
+import CHANNEL_LABEL from '@salesforce/label/c.Channel_label';
+import CONTINUE_LABEL from '@salesforce/label/c.Continue_label';
+import SCHEDULE_LABEL from '@salesforce/label/c.Schedule_label';
+import ENTER_MEDIA_URL_LABEL from '@salesforce/label/c.Enter_Media_URL_label';
+import MESSAGE_BODY_LABEL from '@salesforce/label/c.Message_Body_label';
+import NAME_LABEL from '@salesforce/label/c.Name_label';
+import SELECT_PHONE_FIELD_LABEL from '@salesforce/label/c.Select_Phone_Field_label';
+import SELECT_TEMPLATE_LABEL from '@salesforce/label/c.Select_Template_label';
+import SEND_THIS_MESSAGE_ON_LABEL from '@salesforce/label/c.Send_this_message_on_label';
+import SENDING_ON_LABEL from '@salesforce/label/c.Sending_on_label';
+import START_YOUR_CONVERSATION_LABEL from '@salesforce/label/c.Start_your_conversation_label';
+
+ 
+export const label = {
+    OPTION_SMS_LABEL,
+    OPTION_WHATSAPP_LABEL,
+    SAVE_LABEL,
+    CANCEL_LABEL,
+    CLOSE_LABEL,
+    CHANNEL_LABEL,
+    CONTINUE_LABEL,
+    SCHEDULE_LABEL,
+    ENTER_MEDIA_URL_LABEL,
+    MESSAGE_BODY_LABEL,
+    NAME_LABEL,
+    SELECT_PHONE_FIELD_LABEL,
+    SELECT_TEMPLATE_LABEL,
+    SEND_THIS_MESSAGE_ON_LABEL,
+    SENDING_ON_LABEL,
+    START_YOUR_CONVERSATION_LABEL
+};
